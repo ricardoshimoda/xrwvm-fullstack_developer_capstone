4b449fd87e70c7b40e45db4f17bf837c522c5dde
Ricardo Shimoda Nakasako <rshimoda@gmail.com>
2026-08-19 14:59:17 -0400

    fixing trailing whitespaces

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -94,8 +94,8 @@ def delete_cars(request):
     return JsonResponse({"done": "true"})
 
 
-# Update the `get_dealerships` 
-# render list of dealerships all by default, 
+# Update the `get_dealerships`
+# render list of dealerships all by default,
 # particular state if state is passed
 def get_dealerships(request, state="All"):
     if (state == "All"):
